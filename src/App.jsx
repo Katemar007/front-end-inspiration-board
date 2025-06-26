22950e7228210df97e7be752cd86dc8b99fd7b3a
Tamika Ross <tamika.ross.xu@gmail.com>
2025-06-26 14:52:11 -0500

    Add BoardForm component and lifted board state to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import { useState } from 'react';
+//import axios from 'axios';
 import BoardList from './components/BoardList';
 import CardList from './components/CardList';
 import './App.css'
-import { useState } from 'react';
+import BoardForm from './components/BoardForm';
 import { HashRouter, Route, Routes, Link } from "react-router-dom";
 
 function App() {
   const [boardsData, setBoardsData] = useState([]);
   const [selectedBoard, setSelectedBoard] = useState(null);
 
-  const fakeBoards = [
+  const addNewBoard = (newBoard) => {
+    const updatedBoards = [...boardsData, newBoard];
+    setBoardsData(updatedBoards);
+  };
+
+  /*const fakeBoards = [
     { board_id: 1, title: "Travel", owner: "Danielle" },
     { board_id: 2, title: " Workspace", owner: "Tamika" },
     { board_id: 3, title: "Recipes", owner: "Kate" },
     { board_id: 4, title: "Design", owner: "Solhee" },
-  ];
+  ];*/
   const fakeCards = [
     {id: 1, message: "A", likesCount: 0},
     {id: 2, message: "B", likesCount: 0},
@@ -25,20 +30,18 @@ function App() {
     {id: 6, message: "F", likesCount: 0},
     {id: 7, message: "G", likesCount: 0}
   ]
-  const [selectedBoard, setSelectedBoard] = useState(null);
+
   // const [cardData, setCardData] = useState([]); 
   // // Need to get info from db based on which board is selected.
   // // use that information to get initial cardData
   return (
     <div className="App">
       <h1>Inspiration Board</h1>
+      <BoardForm onBoardSubmit={addNewBoard} />
       <BoardList
-        boards={boardsData.length > 0 ? boardsData : fakeBoards}
+        boards={boardsData}
         selectedBoard={selectedBoard}
-        onBoardSelect={(board) => {
-          console.log("Selected:", board);
-          setSelectedBoard(board);
-        }}
+        onBoardSelect={setSelectedBoard}
       />
       <CardList
         cards={fakeCards}
