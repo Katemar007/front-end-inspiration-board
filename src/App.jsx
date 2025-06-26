421e39e928d1426a76ad44300dec9e5456647846
Tamika Ross <tamika.ross.xu@gmail.com>
2025-06-26 08:34:22 -0500

    added useeffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import BoardList from './components/BoardList';
 import './App.css'
-import { useState } from 'react';
+
+const BASE_URL = import.meta.env.VITE_APP_BACKEND_URL;
+
 
 function App() {
+  const [boardsData, setBoardsData] = useState([]);
+  const [selectedBoard, setSelectedBoard] = useState(null);
+
   const fakeBoards = [
     { board_id: 1, title: "Travel", owner: "Danielle" },
     { board_id: 2, title: " Workspace", owner: "Tamika" },
@@ -10,19 +17,34 @@ function App() {
     { board_id: 4, title: "Design", owner: "Solhee" },
   ];
 
-  const [selectedBoard, setSelectedBoard] = useState(null);
+  useEffect(() => {
+    console.log("Fetching from:", `${BASE_URL}/boards`);
+    axios.get(`${BASE_URL}/boards`)
+      .then((response) => {
+        console.log("Response:", response.data);
+        setBoardsData(response.data); // If this works, switch to this in UI
+      })
+      .catch((error) => {
+        console.error("Error fetching boards:", error);
+      });
+  }, []);
+
+  const handleBoardSelect = (board) => {
+    setSelectedBoard(board);
+  };
+
+
+  
  
 
   return (
     <div className="App">
       <h1>Inspiration Board</h1>
       <BoardList
-        boards={fakeBoards}
+        boards={boardsData.length > 0 ? boardsData : fakeBoards}
         selectedBoard={selectedBoard}
-        onBoardSelect={(board) => {
-          console.log("Selected:", board);
-          setSelectedBoard(board);
-        }}
+        onBoardSelect={handleBoardSelect}
+        
       />
     </div>
   );
