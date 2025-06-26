bf0e0131e48d0cf96dc96d8801ae94d6ffdad900
Solhee <ellen.solhee.jin@gmail.com>
2025-06-25 23:23:53 -0700

    Implemented basic card without some functionalities; waiting to add once able to sync with db. Board and Cards currently displayed on same page using mock-up data.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import BoardList from './components/BoardList';
+import CardList from './components/CardList';
 import './App.css'
 import { useState } from 'react';
+import { HashRouter, Route, Routes, Link } from "react-router-dom";
 
 function App() {
   const fakeBoards = [
@@ -9,10 +11,19 @@ function App() {
     { board_id: 3, title: "Recipes", owner: "Kate" },
     { board_id: 4, title: "Design", owner: "Solhee" },
   ];
-
+  const fakeCards = [
+    {id: 1, message: "A", likesCount: 0},
+    {id: 2, message: "B", likesCount: 0},
+    {id: 3, message: "C", likesCount: 0},
+    {id: 4, message: "D", likesCount: 0},
+    {id: 5, message: "E", likesCount: 0},
+    {id: 6, message: "F", likesCount: 0},
+    {id: 7, message: "G", likesCount: 0}
+  ]
   const [selectedBoard, setSelectedBoard] = useState(null);
- 
-
+  // const [cardData, setCardData] = useState([]); 
+  // // Need to get info from db based on which board is selected.
+  // // use that information to get initial cardData
   return (
     <div className="App">
       <h1>Inspiration Board</h1>
@@ -24,6 +35,9 @@ function App() {
           setSelectedBoard(board);
         }}
       />
+      <CardList
+        cards={fakeCards}
+      ></CardList>
     </div>
   );
 }
