3246d345d0da5f319e83b54858c66eebcb9b052b
KateM <ekaterinamarantidi@gmail.com>
2025-07-03 01:26:34 -0700

    Changed base at vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/front-end-inspiration-board/',
+  base: '/',
   plugins: [react()],
 })
